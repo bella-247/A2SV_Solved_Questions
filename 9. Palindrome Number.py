de7948b0eb2523c903a63282dabf80bb6992e738
Abel Mekonnen <abelmekonen247@gmail.com>
2026-02-04 16:28:37 +0300

    Add implementation of palindrome number checker

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        x_copy = x
+        reverse = 0
+        while x != 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x //= 10
+
+        return x_copy == reverse
+    
